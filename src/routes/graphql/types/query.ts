8e77c06707ca78521a744580b92317fd9dcf6b41
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-11-02 02:49:14 +0300

    feat: add basic user graphql implementation

--- src/routes/graphql/types/query.ts
+++ src/routes/graphql/types/query.ts
@@ -2,37 +2,39 @@ import {
   GraphQLEnumType,
   GraphQLFloat,
   GraphQLInt,
-  GraphQLList, GraphQLNonNull,
-  GraphQLObjectType
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
 } from 'graphql/index.js';
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+import { UUIDType } from './uuid.js';
 
 export const createRootQueryType = async (prisma: PrismaClient) => {
   const MemberTypeId = new GraphQLEnumType({
     name: 'MemberTypeId',
     description: 'One of the films in the Star Wars Trilogy',
     values: {
-      BASIC: {
-        value: "BASIC",
-      },
-      BUSINESS: {
-        value: "BUSINESS",
-      }
+      BASIC: { value: 'BASIC' },
+      BUSINESS: { value: 'BUSINESS' },
     },
   });
 
   const MemberType = new GraphQLObjectType({
     name: 'MemberType',
     fields: {
-      id: {
-        type: new GraphQLNonNull(MemberTypeId),
-      },
-      discount: {
-        type: new GraphQLNonNull(GraphQLFloat),
-      },
-      postsLimitPerMonth: {
-        type: new GraphQLNonNull(GraphQLInt),
-      }
+      id: { type: new GraphQLNonNull(MemberTypeId) },
+      discount: { type: new GraphQLNonNull(GraphQLFloat) },
+      postsLimitPerMonth: { type: new GraphQLNonNull(GraphQLInt) },
+    },
+  });
+
+  const User = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+      id: { type: new GraphQLNonNull(UUIDType) },
+      name: { type: new GraphQLNonNull(GraphQLString) },
+      balance: { type: new GraphQLNonNull(GraphQLFloat) },
     },
   });
 
@@ -42,6 +44,10 @@ export const createRootQueryType = async (prisma: PrismaClient) => {
       memberTypes: {
         type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(MemberType))),
         resolve: async () => await prisma.memberType.findMany(),
+      },
+      users: {
+        type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(User))),
+        resolve: async () => await prisma.user.findMany(),
       },
     },
   });

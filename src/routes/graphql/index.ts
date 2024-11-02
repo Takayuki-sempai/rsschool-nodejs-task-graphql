91e9e2fd648cc5039a091a70f31a494a59ad7271
Vasiliy Prokofiev <Takayuki.sempai@gmail.com>
2024-11-02 05:14:57 +0300

    refactor: add lint and format tasks

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,14 +1,14 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import {createGqlResponseSchema, gqlResponseSchema} from './schemas.js';
-import {graphql, GraphQLSchema} from 'graphql';
-import {createRootQueryType} from "./types/query.js";
+import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import { graphql, GraphQLSchema } from 'graphql';
+import { createRootQueryType } from './types/query.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
 
   const handlerSchema = new GraphQLSchema({
-    query: await createRootQueryType(prisma)
-  })
+    query: await createRootQueryType(prisma),
+  });
 
   fastify.route({
     url: '/',
@@ -23,7 +23,7 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       return graphql({
         schema: handlerSchema,
         source: req.body.query,
-        variableValues: req.body.variables
+        variableValues: req.body.variables,
       });
     },
   });
